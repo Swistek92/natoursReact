7b8a8fc6146cab8083cdcda81f54e04520f6b2ea
swistek <swistekxd@gmail.com>
2022-08-10 22:37:30 +0200

    dsa

--- app.js
+++ app.js
@@ -72,8 +72,8 @@ app.use('/api', limiter);
 
 //body parser reading data from body into req.body
 
-app.use(express.json({ limit: '10kb' }));
-app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ extended: true, limit: '5mb' }));
 app.use(cookieParser());
 // pase data  from cookies
 
