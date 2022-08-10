043c40a1d7dca624d4a66624cd44d1b333787b90
swistek <swistekxd@gmail.com>
2022-08-10 10:24:21 +0200

    asd

--- frontend/src/store/user-slice.js
+++ frontend/src/store/user-slice.js
@@ -14,22 +14,28 @@ const initialState = {
 };
 
 // Register user
-export const register = createAsyncThunk('user/register', async (user) => {
-  try {
-    const response = await axios.post(API_URL + 'signup', user);
-    if (response.data) {
-      localStorage.setItem('user', JSON.stringify(response.data));
+export const register = createAsyncThunk(
+  'user/register',
+  async (user, thunkAPI) => {
+    try {
+      const response = await axios.post(API_URL + 'signup', user);
+      if (response.data) {
+        localStorage.setItem('user', JSON.stringify(response.data));
+      }
+      console.log('response.data', response.data);
+      console.log('response', response);
+      return response.data;
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
     }
-
-    return response.data;
-  } catch (error) {
-    const message =
-      (error.response && error.response.data && error.response.data.message) ||
-      error.message ||
-      error.toString();
-    return message;
   }
-});
+);
 
 export const logout = createAsyncThunk('user/logout', async () => {
   await userService.logout();
